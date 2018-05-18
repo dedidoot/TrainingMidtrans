0850d46938e678a72edecd3b36e7194360819a21
Dedi Android <ahmad.hamidi@sebangsa.com>
2018-05-18 13:44:08 +0700

    up

--- app/src/main/java/com/noteplaces/testmidtrans/MainActivity.java
+++ app/src/main/java/com/noteplaces/testmidtrans/MainActivity.java
@@ -2,12 +2,21 @@ package com.noteplaces.testmidtrans;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.midtrans.sdk.corekit.callback.TransactionCallback;
 import com.midtrans.sdk.corekit.callback.TransactionFinishedCallback;
+import com.midtrans.sdk.corekit.core.MidtransSDK;
+import com.midtrans.sdk.corekit.core.TransactionRequest;
 import com.midtrans.sdk.corekit.core.themes.CustomColorTheme;
+import com.midtrans.sdk.corekit.models.TransactionResponse;
 import com.midtrans.sdk.corekit.models.snap.TransactionResult;
 import com.midtrans.sdk.uikit.SdkUIFlowBuilder;
 
+
+/**
+ * documentation midtrans: https://mobile-docs.midtrans.com/pdf/android_midtrans_mobile_doc.pdf
+ **/
 public class MainActivity extends AppCompatActivity {
 
     public static String CLIENT_KEY = "Mid-client-QtVBI2YYoEYWdDbz";
@@ -25,12 +34,32 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onTransactionFinished(TransactionResult result) {
                         // Handle finished transaction here.
+                        Log.wtf("test", "result: " + result.getSource());
                     }
                 }) // set transaction finish callback (sdk callback)
                 .setMerchantBaseUrl(BASE_URL) //set merchant url (required)
                 .enableLog(true) // enable sdk log (optional)
                 .setColorTheme(new CustomColorTheme("#FFE51255", "#B61548", "#FFE51255")) // set theme. it will replace theme on snap theme on MAP ( optional)
                 .buildSDK();
+        //TransactionRequest transactionRequest = new TransactionRequest()
+
+        MidtransSDK.getInstance().paymentUsingMandiriBillPay("", "", new TransactionCallback() {
+            @Override
+            public void onSuccess(TransactionResponse transactionResponse) {
+                Log.e("onSuccess", "=> " + transactionResponse.getBank());
+            }
+
+            @Override
+            public void onFailure(TransactionResponse transactionResponse, String s) {
+                Log.e("onFailure", "=> " + transactionResponse.getApprovalCode());
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                Log.e("onError", "=> " + throwable.getMessage());
+            }
+        });
+
 
     }
 }
